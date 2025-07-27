ee250f92b4666bcf6d665027adb983259951163e
Dasun Dushmantha <dasunathauda99@gmail.com>
2025-07-27 13:27:06 +0530

    Integrate Convex backend for to-do management
    Added Convex backend with todos schema, queries, and mutations for to-do management. Updated app layout to provide Convex context, and installed the convex package. This enables real-time database operations for to-do items in the app.

--- to_do/app/(tabs)/index.tsx
+++ to_do/app/(tabs)/index.tsx
@@ -1,20 +1,19 @@
+
 import useTheme from "@/hooks/useTheme";
 import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 
 export default function Index() {
-const { toggleDarkMode } = useTheme();
+  const { toggleDarkMode } = useTheme();
+
 
   return (
-    <View
-      style={styles.container}
-    >
+    <View style={styles.container}>
       <Text style = {styles.content}>Edit app/index.tsx to edit this screen123.</Text>
       <Text>Welcome to the To-Do App!</Text>
-      <TouchableOpacity>
-        <Text onPress={toggleDarkMode}>Toggle Dark Mode</Text>
+      <TouchableOpacity onPress={toggleDarkMode}>
+        <Text>Toggle Dark Mode</Text>
       </TouchableOpacity>
-        
-     
+       
     </View>
   );
 }
