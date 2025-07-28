1b54d9bbdf3c879da07960d52915679d04970afe
Dasun Dushmantha <dasunathauda99@gmail.com>
2025-07-29 00:40:40 +0530

    Add settings screen sections and new components
    Introduced ProgressStats, Preferences, and DangerZone components to the settings screen for improved user experience. Updated settings.tsx to use these components and enhanced UI with gradients and icons. Also updated dependencies in package.json and package-lock.json.

--- to_do/app/(tabs)/settings.tsx
+++ to_do/app/(tabs)/settings.tsx
@@ -1,11 +1,53 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView } from 'react-native'
+
+import useTheme from '@/hooks/useTheme';
+import { createSettingsStyles } from '@/assets/styles/settings.styles';
+import { LinearGradient } from 'expo-linear-gradient';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Ionicons } from '@expo/vector-icons';
+import ProgressStats from '@/components/ProgressStats';
+import Preferences from '@/components/Preferences';
+import DangerZone from '@/components/DasngerZone';
 
 const SettingsScreen = () => {
+
+  const { colors } = useTheme();
+  
+  const settingsStyles = createSettingsStyles(colors)
+  
+
   return (
-    <View>
-      <Text>SettingsScreen</Text>
-    </View>
+    <LinearGradient colors={colors.gradients.background} style={settingsStyles.container}>
+      
+      <SafeAreaView style={settingsStyles.safeArea}>
+        {/*HEADER*/}
+        <View style={settingsStyles.header}>
+          <View style={settingsStyles.titleContainer}>
+          <LinearGradient colors={colors.gradients.primary} style=
+          {settingsStyles.iconContainer}>
+            <Ionicons name="settings" size={28} color="#fff" />
+          </LinearGradient>
+          <Text style={settingsStyles.title}>Settings</Text>
+        </View>
+        </View>
+
+        <ScrollView
+          style={settingsStyles.scrollView}
+          contentContainerStyle={settingsStyles.content}
+          showsVerticalScrollIndicator={false}>
+
+          <ProgressStats />
+
+          <Preferences />
+
+          <DangerZone />
+
+        </ScrollView>
+          
+
+
+      </SafeAreaView>
+    </LinearGradient>
   )
 }
 
